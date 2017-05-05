d31f5fb49112aec44e2f4e344f648c5af70892b6
nikst <e>
2017-05-05 16:47:12 +0200

    Initial commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+
+    <h1>A place to show the posts.</h1>
+
+    @endsection
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/','PostsController@index');
+Route::get('/posts/create','PostsController@create');
+//Route::get('posts/{post}','PostsController@show');
